c324077de6bca0380b61a71a4c9821091dd2f017
Krishnaswamy Subramanian <jskswamy@gmail.com>
2022-02-17 15:16:41 +0530

    Add example modules

--- example/basic/main.tf
+++ example/basic/main.tf
@@ -1,21 +1,21 @@
 resource "dwarka_building" "main" {
-  name        = "terraform"
+  name        = "basic building"
   description = "from terraform"
   lat         = 13.0827
   lan         = 80.2707
 }
 
-resource "dwarka_floor" "first" {
+resource "dwarka_floor" "ground" {
   building_id = dwarka_building.main.id
-  name        = "first floor"
+  name        = "ground floor"
   description = "from terraform"
   level       = 1
 }
 
-resource "dwarka_room" "one" {
+resource "dwarka_room" "hall" {
   building_id = dwarka_building.main.id
-  floor_id    = dwarka_floor.first.id
-  name        = "main"
-  direction   = "east"
+  floor_id    = dwarka_floor.ground.id
+  name        = "main room"
   description = "from terraform"
+  direction   = "south"
 }
