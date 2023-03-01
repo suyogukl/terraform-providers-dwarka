1fa4e76f458e30dff32dc0d3b1f4c1a11824cdd0
suyogukl <suyogukalkar@Suyogs-MacBook-Pro.local>
2023-03-01 10:36:34 +0530

    Suyog/Sapana | add one bhk module

--- example/basic/main.tf
+++ example/basic/main.tf
@@ -1,21 +1,4 @@
-resource "dwarka_building" "main" {
-  name        = "basic building"
-  description = "from terraform"
-  lat         = 13.0827
-  lan         = 80.2707
+module "onebhk" {
+  source = "./modules/onebhk"
 }
 
-resource "dwarka_floor" "ground" {
-  building_id = dwarka_building.main.id
-  name        = "ground floor"
-  description = "from terraform"
-  level       = 1
-}
-
-resource "dwarka_room" "hall" {
-  building_id = dwarka_building.main.id
-  floor_id    = dwarka_floor.ground.id
-  name        = "main room"
-  description = "from terraform"
-  direction   = "south"
-}
